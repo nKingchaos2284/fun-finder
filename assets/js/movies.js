bf47b701d1d92423bb90a900e64f3eb512c9aff8
Jose Mauricio Cabrera Elias <jmcabrerae@gmail.com>
2023-02-08 00:08:19 -0800

    added some modifications, corrected spelling

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,11 +1,11 @@
 const API_KEY = `7fa03b692f6aa3af85af42b2dba34aee`;
 
-const searchTemperature = () => {
+const searchMovie = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.themoviedb.org/3/movie/550?api_key=7fa03b692f6aa3af85af42b2dba34aee`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayTemperature(data));
+        .then(data => displayMovie(data));
 }
 
 const setInnerText = (id, text) =>{
@@ -17,9 +17,9 @@ const setInnerText = (id, text) =>{
 
 const displayTemperature = temperature => {
     console.log(temperature);
-    setInnerText('city', temperature.name);
-    setInnerText('temp', temperature.main.temp);
-    setInnerText('weather', temperature.weather[0].main);
+    setInnerText('city', movies.name);
+    setInnerText('movies', movies.main.temp);
+   
 
     // movie icon settings 
     const url = `https://api.themoviedb.org/3/movie/550?api_key=7fa03b692f6aa3af85af42b2dba34aee`;
