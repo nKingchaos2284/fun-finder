f496d65b0394fe29da97de128bc2ce184236c5d9
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-02-08 18:23:41 -0800

    added event listeners for recommended cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 var searchBtn = document.querySelector("#searchBtn");
 var resultsEl = document.querySelector("#results");
-var miamiEl = document.querySelector();
-var sandiegoEl = document.querySelector();
-var lasvegasEl = document.querySelector();
-var losangelesEl = document.querySelector();
+var miamiEl = document.querySelector("#miami");
+var sandiegoEl = document.querySelector("#sandiego");
+var lasvegasEl = document.querySelector("#lasvegas");
+var losangelesEl = document.querySelector("#losangeles");
+var newyorkEl = document.querySelector("#newyork");
+var seattleEl = document.querySelector("#seattle");
 
+// function for clicks on search button
 function renderResults() {
     console.log("Results Rendered");
     resultsEl.classList.remove('hidden');
     resultsEl.classList.add('visible');
-
 }
 
+// functions for clicks on recommended cards
+
+// search button event listener
 searchBtn.addEventListener('click', renderResults);
+// recommended card event listeners
+miamiEl.addEventListener('click', );
+sandiegoEl.addEventListener('click', );
+lasvegasEl.addEventListener('click', );
+losangelesEl.addEventListener('click', );
+newyorkEl.addEventListener('click', );
+seattleEl.addEventListener('click', );
