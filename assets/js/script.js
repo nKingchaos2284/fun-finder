f2b2714947112d075fa422ec777ddd35fb925a57
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-02-08 17:31:49 -0800

    fixed up the cards, fixed position for the header, nav bar links fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var searchBtn = document.querySelector("#searchBtn");
 var resultsEl = document.querySelector("#results");
+var miamiEl = 
 
 function renderResults() {
     console.log("Results Rendered");
     resultsEl.classList.remove('hidden');
     resultsEl.classList.add('visible');
+
 }
 
 searchBtn.addEventListener('click', renderResults);
